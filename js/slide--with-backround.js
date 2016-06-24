6cc99ca47e121cb6891b125636449285782f6243
Kai Wittmann <kaisky89@gmail.com>
2016-06-24 16:27:58 +0200

    section 3 almost done. Still a few problems in darkening the background

--- js/slide--with-backround.js
+++ js/slide--with-backround.js
@@ -2,4 +2,7 @@ $(document).ready(function (){
   $('.slide--with-background').wrapInner(
     '<div class="slide--with-background__inner-div"></div>'
   );
+  $('.slide--with-background__inner-div').height(
+    $(this).parent().height()
+  );
 });
