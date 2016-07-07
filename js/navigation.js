44f6dc965a5b6f5bc5ba6e65b8142d6e53e40d23
Marvin Klimm <marvin_christian.klimm@smail.fh-koeln.de>
2016-07-07 18:35:01 +0200

    padding bearbeitet

--- js/navigation.js
+++ js/navigation.js
@@ -1,26 +1,36 @@
 $( document ).ready(function(){
-  var docked = false;
-  var menu = $('#navbar-starters');
-  var init = menu.offset().top;
+  navbar();
+  $( window ).resize(function() {
+    navbar();
+  });
 
-  $(window).scroll(function()
-    {
-      if (!docked && (menu.offset().top - $("body").scrollTop() < 50))
-      {
-          menu.css({
-              position : "fixed",
-              top: 50,
-          });
-          docked = true;
-      }
-      else if(docked && ($("body").scrollTop()+50) <= init)
-      {
-          menu.css({
-              position : "absolute",
-              top: init + 'px',
-          });
-
-          docked = false;
-        }
-    });
 });
+
+function navbar() {
+  if( $(window).width() > 767 ) {
+    var docked = false;
+    var menu = $('#navbar-starters');
+    var init = menu.offset().top;
+
+    $(window).scroll(function()
+      {
+        if (!docked && (menu.offset().top - $("body").scrollTop() < 50))
+        {
+            menu.css({
+                position : "fixed",
+                top: 50,
+            });
+            docked = true;
+        }
+        else if(docked && ($("body").scrollTop()+50) <= init)
+        {
+            menu.css({
+                position : "absolute",
+                top: init + 'px',
+            });
+
+            docked = false;
+          }
+      });
+  }
+}
