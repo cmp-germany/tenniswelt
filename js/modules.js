6cbf8977d2df5cd0e1e72f6b68feec8dbd8d33d0
Kai Wittmann <kaisky89@gmail.com>
2016-07-12 14:47:55 +0200

    JS nach Ajax ausführen

--- js/modules.js
+++ js/modules.js
@@ -1,9 +1,15 @@
-$("div[data-module]").each(function(){
-  var moduleName = $(this).attr("data-module")
-  $.get({
-    url: './modules/' + moduleName,
-    success: function(data) {
-      $(this).append(data);
-    }.bind(this)
+$(document).ready(function() {
+  $("div[data-module]").each(function(){
+    var moduleName = $(this).attr("data-module")
+    $.get({
+      url: './modules/' + moduleName,
+      success: function(data) {
+        $(this).append(data);
+        var additionalJs = $(this).attr('data-additional-js');
+        if (additionalJs) {
+          $.getScript(additionalJs);
+        }
+      }.bind(this)
+    });
   });
 });
