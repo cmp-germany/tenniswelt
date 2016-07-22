0692a020ebf586c0d522119bc30d68712a04806a
Marvin Klimm <marvin_christian.klimm@smail.fh-koeln.de>
2016-07-22 15:23:25 +0200

    matching setting in radio buttons

---
+++ js/matching-setting.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+  displayOption();
+  $('.radio-event').change(function() {
+    displayOption();
+  });
+});
+
+function displayOption() {
+  if($('#cm').is(':checked')){
+    console.log("asd");
+    $('.radio-hide-away').addClass('display-option');
+  }
+
+  else {
+    $('.radio-hide-away').removeClass('display-option');
+  }
+}
