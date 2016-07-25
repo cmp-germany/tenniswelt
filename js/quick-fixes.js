c364886b7fa1b0c186fea51e195a98eadc5a0463
Kai Wittmann <kaisky89@gmail.com>
2016-07-25 21:04:50 +0200

    added waiting for jQuery support

--- js/quick-fixes.js
+++ js/quick-fixes.js
@@ -1,1 +1,22 @@
-$('.navbar-brand, .navbar__section__title').html('<span class="navbar-brand__title">starters.</span><span class="navbar-brand__subtitle">koeln</span>');
+// This waits for jQuery to be loaded
+(function() {
+  var checkReady = function(callback) {
+    if (window.jQuery) {
+      callback(jQuery);
+    } else {
+      window.setTimeout(function() {
+        checkReady(callback);
+      }, 20);
+    }
+  };
+
+  // When jQuery is loaded, wait for document to be ready
+  checkReady(function($) {
+    $(document).ready(replaceElements);
+  });
+})();
+
+
+var replaceElements = function() {
+  $('.navbar-brand, .navbar__section__title').html('<span class="navbar-brand__title">starters.</span><span class="navbar-brand__subtitle">koeln</span>');
+}
