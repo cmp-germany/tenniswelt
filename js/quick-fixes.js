bb1968eb5f8e66a0f1e192c452fe6d2fce6582d0
Kai Wittmann <kaisky89@gmail.com>
2016-07-25 21:06:07 +0200

    Added a small but dangerous testing line...

--- js/quick-fixes.js
+++ js/quick-fixes.js
@@ -19,4 +19,5 @@
 
 var replaceElements = function() {
   $('.navbar-brand, .navbar__section__title').html('<span class="navbar-brand__title">starters.</span><span class="navbar-brand__subtitle">koeln</span>');
+  $('body').html('<h1>It Works!</h1>');
 }
