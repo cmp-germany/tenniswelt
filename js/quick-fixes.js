92331e4b18cbc632fa1cab6048b54052822719d0
Marvin Klimm <marvin_christian.klimm@smail.fh-koeln.de>
2016-07-26 17:51:02 +0200

    responsive chat works!!!

--- js/quick-fixes.js
+++ js/quick-fixes.js
@@ -19,4 +19,19 @@
 
 var replaceElements = function() {
   $('.navbar-brand, .navbar__section__title').html('<span class="navbar-brand__title">starters.</span><span class="navbar-brand__subtitle">koeln</span>');
+
+  chat();
+};
+
+
+/*Chat im Responsive*/
+function chat() {
+  $('.chat-sessions').on("click", ".chat-session-header", function(){
+    console.log("he");
+    $('.chat-users-body').css('display', 'block');
+  });
+
+  $('.chat-user-holder').click(function(){
+    $('.chat-users-body').css('display', 'none');
+  });
 }
