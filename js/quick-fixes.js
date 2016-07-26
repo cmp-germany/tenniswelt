d57de6290e056398472af82df4f835f2ca2ff5d6
Kai Wittmann <kaisky89@gmail.com>
2016-07-26 18:50:30 +0200

    next Test

--- js/quick-fixes.js
+++ js/quick-fixes.js
@@ -1,3 +1,23 @@
+/*Chat im Responsive*/
+var chatQuickFixes = function () {
+  $('.chat-sessions').on("click", ".chat-session-header", function(){
+    console.log("he");
+    $('.chat-users-body').css('display', 'block');
+  });
+
+  $('.chat-user-holder').click(function(){
+    $('.chat-users-body').css('display', 'none');
+  });
+}
+
+
+
+var replaceElements = function() {
+  $('.navbar-brand, .navbar__section__title').html('<span class="navbar-brand__title">starters.</span><span class="navbar-brand__subtitle">koeln</span>');
+
+  chatQuickFixes();
+};
+
 // This waits for jQuery to be loaded
 (function() {
   var checkReady = function(callback) {
@@ -15,23 +35,3 @@
     $(document).ready(replaceElements);
   });
 })();
-
-
-var replaceElements = function() {
-  $('.navbar-brand, .navbar__section__title').html('<span class="navbar-brand__title">starters.</span><span class="navbar-brand__subtitle">koeln</span>');
-  console.log('Dies ist ein Test');
-  chat();
-};
-
-
-/*Chat im Responsive*/
-function chat() {
-  $('.chat-sessions').on("click", ".chat-session-header", function(){
-    console.log("he");
-    $('.chat-users-body').css('display', 'block');
-  });
-
-  $('.chat-user-holder').click(function(){
-    $('.chat-users-body').css('display', 'none');
-  });
-}
