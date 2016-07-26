2c0dfd1a1e0af675a86d08f8b969e1a74aa8b184
Kai Wittmann <kaisky89@gmail.com>
2016-07-26 19:15:06 +0200

    fixed problem with javascript injection

--- js/quick-fixes.js
+++ js/quick-fixes.js
@@ -2,11 +2,17 @@
 var chatQuickFixes = function () {
   console.log("chatQuickFixes is called!");
   $('.chat-sessions').on("click", ".chat-session-header", function(){
-    console.log("he");
     $('.chat-users-body').css('display', 'block');
   });
 
-  $('.chat-user-holder').click(function(){
+  // chat is appearing later than document.onLoad.
+  // So, we need to get an event listener for
+  // future things.
+  // see http://api.jquery.com/live/ (which is deprecated)
+  // so, better, proper use of on():
+  // http://stackoverflow.com/questions/8021436/turning-live-into-on-in-jquery
+
+  $('.chat-sessions').on("click", ".chat-user-holder", function(){
     $('.chat-users-body').css('display', 'none');
   });
 }
