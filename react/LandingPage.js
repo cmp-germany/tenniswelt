046d6eb9cf6a00c7565c30da1c60ec8aaaf2fc85
Kai Wittmann <kaisky89@gmail.com>
2016-08-03 14:54:47 +0200

    Landing Page Data Modell bauen

---
+++ react/LandingPage.js
@@ -0,0 +1,51 @@
+var contentLandingPage = [
+  {
+    "module": "TitleWithRegisterForm",
+    "contents": {
+      "title": "Treten Sie jetzt mit der Kölner Gründerszene in Kontakt!",
+      "text":
+        "Werden Sie Teil der Startup Szene und treten Sie einem internationalem Portal bei. Hier finden Sie alle Möglichkeiten, die Sie für Ihr aufstrebendes Unternehmen brauchen: Kommunizieren Sie mit Gleichgesinnten, tauschen Sie Ihr Wissen aus, finden Sie Ihre Kunden und entdecken Sie die Finanzierungsmöglichkeiten, die Ihrer Geschäftsidee den nötigen Boost verleihen.",
+      "formTitle": "Kostenlos registrieren und ein Jahr unverbindlich testen.",
+      "buttonAdditionalText": 'Per Klick auf "Jetzt Mitglied werden" stimmen Sie unseren AGB und Datenschutzrichtlinien zu.',
+      "buttonText": "Jetzt Mitglied werden"
+    },
+    "pictures": {
+      "background": {
+        "src": "../gfx/landing-hintergrund.jpg",
+        "position-x": "100%",
+        "position-y": "50%"
+      },
+      "motiv": {
+        "src": "gfx/flieger-junge.png"
+      }
+    },
+    "inputFields": [
+      {
+        "inputType": "text",
+        "label": "Name des Unternehmens"
+      },
+      {
+        "inputType": "select",
+        "options": [
+          "Kategorie 1",
+          "Kategorie 2",
+          "Kategorie 3",
+          "Kategorie 4"
+        ]
+      },
+      {
+        "inputType": "select",
+        "options": [
+          "Branche 1",
+          "Branche 2",
+          "Branche 3",
+          "Branche 4"
+        ]
+      },
+      {
+        "inputType": "text",
+        "label": "Benutzername"
+      }
+    ]
+  }
+];
