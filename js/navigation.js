ab0dfbf208b703e6d960040f8fd419c1572fd4e0
Kai Wittmann <k.wittmann@cmp-germany.com>
2016-11-09 12:53:38 +0100

    ✓ test in IE

--- js/navigation.js
+++ js/navigation.js
@@ -8,7 +8,7 @@ $( document ).ready(function(){
 });
 
 function initReactComponents() {
-  ReactDOM.render(
+  window.reactFriendRequestsApp = ReactDOM.render(
     <FriendRequestsModule
       userId={window.currentUserId}
       currentLanguage={window.currentLanguage}
