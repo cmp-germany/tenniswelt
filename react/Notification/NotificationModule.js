4aad79d7632ef444f563861c530f68eee2303a7e
Kai Wittmann <k.wittmann@cmp-germany.com>
2016-11-10 14:59:27 +0100

    ✓ create an easy-to-use interface

--- react/Notification/NotificationModule.js
+++ react/Notification/NotificationModule.js
@@ -5,15 +5,27 @@ const materialDesignLite  = require('material-design-lite');
 var NotificationModule = react.createClass({
   mixins: [MaterialDesignMixin],
 
-  test: function() {
-    var notification = document.querySelector('.mdl-js-snackbar');
-    notification.MaterialSnackbar.showSnackbar({message: 'Image Uploaded'});
+  notify: function(notification) {
+    var finalNotification;
+
+    // if notification is just a String, create an object out of it
+    if (typeof notification === 'string' || notification instanceof String) {
+      finalNotification = {message: notification};
+    }
+
+    // otherwise, just pass the object as it is given. MDL will handle proper
+    // error handling, if there is given something stupid here.
+    else {
+      finalNotification = notification;
+    }
+
+    this.snackbar.MaterialSnackbar.showSnackbar(finalNotification);
   },
 
   render: function() {
     return (
       <div>
-        <div className="mdl-snackbar mdl-js-snackbar">
+        <div className="mdl-snackbar mdl-js-snackbar" ref={(snackbar) => this.snackbar = snackbar}>
           <div className="mdl-snackbar__text"></div>
           <button type="button" className="mdl-snackbar__action"></button>
         </div>
