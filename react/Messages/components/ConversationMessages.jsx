75a8658ec6aedf06fc1c3f66cf6c8ec4865b8e6e
kaisky89 <kaisky89@gmail.com>
2016-12-19 17:54:31 +0100

    fix eventlistener issues on UserStore

--- react/Messages/components/ConversationMessages.jsx
+++ react/Messages/components/ConversationMessages.jsx
@@ -45,9 +45,26 @@ var ConversationMessages = React.createClass({
   getStateFromStore: function() {
     var currentConversation = currentConversationStore.getConversation();
     var messages = currentConversation ? currentConversation.messages : null;
+
+    // collect all different userIds, which are appearing in this Conversation.
+    var userIds = [];
+    if (messages) {
+      _.forEach(messages, function(element){
+        if (!_.find(userIds, function(u){return u == element.user})) {
+          userIds.push(element.user);
+        }
+      });
+    }
+
+    // load the users of the collected userIds
+    var users = {};
+    _.forEach(userIds, function(userId) {
+      users[userId] = userStore.getUser(userId);
+    });
+
     return {
       messages,
-      users: userStore.getUsers(),
+      users,
       currentUser: currentUserStore.getCurrentUser(),
       isLoaded: currentConversationStore.isLoaded(),
       isLoading: currentConversationStore.isLoading(),
@@ -68,11 +85,12 @@ var ConversationMessages = React.createClass({
     var renderedConversationMessages;
     if (this.state.messages) {
       renderedConversationMessages = this.state.messages.map(function(element, index){
+        var user = users[element.user];
         return (
           <ConversationMessage
             message={element}
             key={index}
-            user={users[element.user]}
+            user={user}
             currentUser={currentUser}
             currentLanguage={currentLanguage}
           />
