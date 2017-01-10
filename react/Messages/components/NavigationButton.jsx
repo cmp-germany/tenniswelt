f8966369d9fe6922f0a9fe950f686d06f7f6223e
Kai Wittmann <k.wittmann@cmp-germany.com>
2017-01-10 16:16:37 +0100

    ✓ Navigation Buttons have errors

--- react/Messages/components/NavigationButton.jsx
+++ react/Messages/components/NavigationButton.jsx
@@ -1,10 +1,11 @@
 const React  = require('react');
+const currentViewActions = require('../actions/CurrentViewActions');
 
 
 var NavigationButton = React.createClass({
   render: function() {
     return (
-      <a href="script:void(0)" onClick={() => {currentViewActions.navigateTo(this.props.navigateTo);}}>
+      <a href="javascript:void(0)" onClick={() => {currentViewActions.navigateTo(this.props.navigateTo);}}>
         <i className="material-icons material-icons--clickable">{this.props.icon}</i>
       </a>
     )
