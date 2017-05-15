3c9294dc4ca6c9de187356797b984a01f8e49648
Kai Wittmann <k.wittmann@cmp-germany.com>
2017-05-15 17:34:34 +0200

    working on navbar json compilation

--- convert-navbar-json.js
+++ convert-navbar-json.js
@@ -3,6 +3,24 @@ var navbarMenu = require('./data/navbarMenu.json');
 var jsonfile   = require('jsonfile');
 
 var compatibleMenu = { "navbarMenu" : [] };
+var categoriesMenu = { "categories" : [] };
+
+var createCategoryObject = (key, val) => {
+  if (_.isString(val)) {
+    return {
+      "name": key
+    };
+  }
+
+  var subelements = _.map(val, (val, key) => {
+    return createCategoryObject(key, val);
+  });
+
+  return {
+    "name": key,
+    "subelements": subelements
+  };
+}
 
 var createMenuObject = (key, val) => {
   if (_.isString(val)) {
@@ -10,6 +28,16 @@ var createMenuObject = (key, val) => {
       "name": key,
       "href": val
     };
+  }
+
+  if (key == "Stichwortsuche") {
+    categoriesMenu.categories = _.map(val, (val, key) => {
+      return createCategoryObject(key, val);
+    });
+    return {
+      "name": key,
+      "href": "#categoryList"
+    }
   }
 
   var subelements = _.map(val, (val, key) => {
@@ -27,8 +55,18 @@ compatibleMenu.navbarMenu = _.map(navbarMenu, (val, key) => {
   return createMenuObject(key, val);
 });
 
-var file = './data/navbarMenu--compatible.json';
+var file1 = './data/navbarMenu--compatible.json';
 
-jsonfile.writeFile(file, compatibleMenu, function (err) {
-  console.error(err)
+jsonfile.writeFile(file1, compatibleMenu, function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
+
+var file2 = './data/navbarMenu--categories.json';
+
+jsonfile.writeFile(file2, categoriesMenu, function (err) {
+  if (err) {
+    console.error(err);
+  }
 });
